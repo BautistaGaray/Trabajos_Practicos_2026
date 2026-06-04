0f54daeec7c3416b4b961d82ce4a94bdf12e52a4
alumno <51085965@ivt.edu.ar>
2026-06-04 08:18:21 -0300

    Java

--- TP5/app.js
+++
@@ -1,8 +0,0 @@
-let variable1 = 67
-let variable2 = 1024
-let Resultado
-
-Resultado = variable1 + variable2
-
-console.log(Resultado);
-alert (Resultado)
---
+++ TP5/index.js
@@ -0,0 +1,31 @@
+let suma
+let resta
+let multiplicacion
+let division
+suma = (20+20)
+resta = (67-41)
+multiplicacion = (2*54)
+division = (45/9)
+alert("El resultado de la suma es: " + suma)
+alert("El resultado de la resta es: " + resta)
+console.log("El resultado de la multiplicación es: " + multiplicacion)
+console.log("El resultado de la división es: " + division)
+let nombreAlumno = "Cosme"
+let apellidoAlumno = "Fulanito"
+let edadAlumno = 67
+console.log ("El alumno: "+nombreAlumno+" "+apellidoAlumno+" "+"tiene "+edadAlumno+" "+"años.")
+let precio1 = 123
+let precio2 = 45
+let precio3 = 89
+let total
+total = (precio1+precio2+precio3)
+console.log ("El total a pagar es de: "+total)
+let comidaFavorita = "milanesa con puré"
+let colorFavorito = "azul"
+let numeroFavorito = 12
+console.log ("Mi comida favorita es la "+comidaFavorita+", mi color favorito es el "+colorFavorito+" y mi número favorito es el "+numeroFavorito)
+let pais
+pais = "Argentina"
+console.log (pais)
+pais = "Brasil"
+console.log (pais)
