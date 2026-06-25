5a75fa2ebc803f2e63e18df8450c12dd8e6edf03
alumno <51085965@ivt.edu.ar>
2026-06-25 08:50:51 -0300

    Cambios Finales

--- TP6/app.js
+++ TP6/app.js
@@ -1,7 +1,35 @@
 //Variables
+
 let titulo = document.querySelector ("h1")
 let parrafo = document.querySelector ("p")
 let botonCambiarFondo = document.querySelector ("#BotonCambioFondo")
 let botonCambiarTexto = document.querySelector ("#BotonCambioTexto")
-let botonCambiarColor = document.querySelector ("#BotonCambioColor")
+let botonCambiarColorLetra = document.querySelector ("#BotonCambioColorLetra")
 let botonCambiarLetra = document.querySelector ("#BotonCambioLetra")
+let botonReiniciar = document.querySelector ("#BotonReinicio")
+let botonCambiarTamaño = document.querySelector ("#BotonCambioTamañoLetra")
+
+//Funcionamiento de los botones
+
+botonCambiarFondo.onclick = function () {
+    parrafo.style.backgroundColor = "navajowhite"
+}
+botonCambiarTamaño.onclick = function () {
+    parrafo.style.fontSize = "30px"
+}
+botonCambiarTexto.onclick = function () {
+    parrafo.textContent = "Perfecto, cambiaste el contenido del parrafo. Prueba a reiniciar los cambios o probar otros botones"
+}
+botonCambiarLetra.onclick = function () {
+    parrafo.style.fontFamily = "Impact"
+}
+botonCambiarColorLetra.onclick = function () {
+    parrafo.style.color = "gray"
+}
+botonReiniciar.onclick = function () {
+    parrafo.style.backgroundColor = "beige"
+    parrafo.style.fontSize = "medium"
+    parrafo.textContent = "En esta página, vas a poder realizar distintos cambios a la página web, con los botones interactivos. Intenta probar alguno."
+    parrafo.style.fontFamily = "Arial"
+    parrafo.style.color = "darkgoldenrod"
+}
